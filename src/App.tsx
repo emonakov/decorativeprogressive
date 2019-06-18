d8a1882370900022c95ac40b01fd706ff58669d4
Eugene Monakoff <montemonakov@gmail.com>
2019-06-18 22:11:11 +0100

    new styles, added hero

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import MainNav from './components/rightbar/Menu';
+import Screen from './components/leftbar/Screen';
+import CenterBar from './components/centerbar/CenterBar'
+import Hero from './components/hero/Hero';
 import './App.css';
 
 const App: React.FC = ({ children }) => {
 	return (
 		<section className="wrapper">
+			<Hero />
+			<Screen />
+			<CenterBar />
+			<MainNav />
 			{children}
 		</section>
 	);
---
+++ src/components/hero/Hero.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './styles/hero.css';
+
+const Hero: React.FC = () => (
+    <section className="hero"></section>
+);
+
+export default Hero;
