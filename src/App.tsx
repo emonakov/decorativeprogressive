2127df0eeb2e20c7b7567f3a772cabccdda031a9
Eugene Monakoff <montemonakov@gmail.com>
2019-06-22 20:06:35 +0100

    Router Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import './shared/libs/FontAwesomeLib';
 
@@ -8,14 +9,34 @@ import CenterBar from './components/Centerbar'
 import Hero from './components/Hero';
 import './App.css';
 
-const App: React.FC = ({ children }) => {
+const Home: React.FC = () => (
+	<>
+		<Hero />
+		<LeftBar />
+		<CenterBar>
+			<Link to="/test">
+				<p>TEST</p>
+			</Link>
+		</CenterBar>
+		<RightBar />
+	</>
+);
+
+const Test: React.FC = () => (
+	<h1>
+		<Link to="/">
+			<p>TEST</p>
+		</Link>
+	</h1>
+);
+
+const App: React.FC = () => {
 	return (
 		<section className="wrapper">
-			<Hero />
-			<LeftBar />
-			<CenterBar />
-			<RightBar />
-			{children}
+			<Router>
+				<Route path="/" exact component={Home} />
+				<Route path="/test" component={Test} />
+			</Router>
 		</section>
 	);
 }
--- src/components/centerbar/index.tsx
+++ src/components/centerbar/index.tsx
@@ -3,8 +3,10 @@ import React from 'react';
 import { CENTERBAR_CSS_CLASS } from './styles/constants';
 import './styles/center-bar.css';
 
-const CenterBar: React.FC = () => (
-    <section className={CENTERBAR_CSS_CLASS}></section>
+const CenterBar: React.FC = ({ children }) => (
+    <section className={CENTERBAR_CSS_CLASS}>
+        {children}
+    </section>
 );
 
 export default CenterBar;
