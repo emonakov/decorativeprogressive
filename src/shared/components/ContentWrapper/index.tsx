d43fc437cb8c28c43b5082a99797d35fbeeca55f
Eugene Monakoff <ymonakov@gmail.com>
2019-08-13 16:29:45 +0100

    semantic html fix

--- src/shared/components/ContentWrapper/index.tsx
+++ src/shared/components/ContentWrapper/index.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-const ContentWrapper = styled.section`
+const ContentWrapper = styled.article`
     grid-area: content;
     padding: ${({ theme }) => theme.contentPadding};
     font-size: 1.1rem;
