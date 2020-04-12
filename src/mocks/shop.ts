6ccc67efa8c6916339bec51239b7e2e371ffec07
Eugene Monakov <ymonakov@gmail.com>
2020-04-12 01:02:01 +0100

    Dockerfile

--- src/mocks/shop.ts
+++ src/mocks/shop.ts
@@ -50,6 +50,28 @@ export default async () => ({
                 ],
             },
         },
+        {
+            title: 'Unnamed carpet',
+            description: `
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nunc nulla, pharetra quis consectetur et,
+            placerat vitae ex. Praesent facilisis orci vitae dui placerat, a ultrices tortor lacinia. Ut iaculis ex
+            turpis, quis aliquet tellus pretium sed. Duis accumsan, orci sed ultrices facilisis, dui dolor maximus elit,
+            sit amet cursus leo metus iaculis sem. Ut vitae neque varius, porttitor nunc volutpat, ultricies massa.
+            Fusce quis bibendum massa. Aliquam diam sapien, suscipit vitae eros id, pharetra tempor nulla. Mauris eget
+            massa suscipit lacus gravida volutpat quis vitae est. Vestibulum vel convallis enim. Donec a orci nec sem
+            euismod accumsan non id orci. Pellentesque rutrum, nisl sed mattis posuere, urna lacus bibendum ex, quis
+            lobortis orci mi feugiat enim. Donec elementum posuere lorem, et efficitur neque bibendum id. Aenean
+            suscipit nec ipsum sit amet pellentesque. Proin non erat eget lectus vehicula eleifend. Mauris neque dui,
+            elementum quis dui eu, auctor cursus tellus. Sed at posuere lorem, vitae bibendum neque.`,
+            price: 200,
+            id: 3,
+            images: {
+                main: 'main.jpg',
+                add: [
+                    'img1.jpg',
+                ],
+            },
+        },
     ],
     errors: [],
     loading: false,
