0f43ce2e45f3cf67f45daded492a003cfe9a1eb7
Eugene Monakov <evgenii.monakov@auto1.com>
2020-11-20 17:06:48 +0100

    Database init test 3

--- api/hello.js
+++ api/hello.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
 module.exports = (req, res) => {
-    // const { name = 'World' } = req.query;
+    const { name = 'World' } = req.query;
     db.on('error', (args) => res.status(200).send(JSON.stringify(args)));
     db.once('open', (args) => res.status(200).send(JSON.stringify(args)));
-    // res.status(200).send(`Hello, ${name}!`);
+    res.status(200).send(`Hello, ${name}!`);
 };
