202dad26344ba1286d5ce1202b24a6e6afb2d915
Eugene Monakov <evgenii.monakov@auto1.com>
2020-11-25 15:37:08 +0100

    another mongoose test

--- api/hello.js
+++ api/hello.js
@@ -1,19 +1,33 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const uri = process.env.MONGO_URL;
-mongoose.connect(uri, {useNewUrlParser: true});
+mongoose.connect(uri, { useNewUrlParser: true });
 
-module.exports = (req, res) => {
-    // const { name = 'World' } = req.query;
-    const db = mongoose.connection;
-    db.on('error', (err) => {
-        res.status(200).send(JSON.stringify(err));
-    });
-    db.once('open', function() {
-        res.status(200).send('Weird thing');
-    });
+const productSchema = new mongoose.Schema({
+    title: String,
+    price: number,
+    createdAt: Date
+    productAssets: String
+    images: {
+        main: String,
+        add: [String]
+    }
+});
 
-    setTimeout(() => {
-        res.status(200).send('Nothing happened');
-    }, 5000)
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = async (req, res) => {
+  // const { name = 'World' } = req.query;
+  const db = mongoose.connection;
+  db.on("error", (err) => {
+    res.status(200).send(JSON.stringify(err));
+  });
+  db.once("open", async () {
+    const products = await Product.find();
+    res.status(200).send(JSON.stringify(products));
+  });
+
+  setTimeout(() => {
+    res.status(200).send("Nothing happened");
+  }, 5000);
 };
