b18528110a10ee9a4d1a4ad0f22b9f5885b65990
Eugene Monakov <evgenii.monakov@auto1.com>
2020-12-25 15:22:46 +0100

    eslint and database fix

--- api/hello.js
+++ api/hello.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const uri = process.env.MONGO_URL;
 mongoose.connect(uri, { useNewUrlParser: true });
@@ -10,24 +10,24 @@ const productSchema = new mongoose.Schema({
     productAssets: String,
     images: {
         main: String,
-        add: [String]
-    }
+        add: [String],
+    },
 });
 
 const Product = mongoose.model('Product', productSchema);
 
 module.exports = async (req, res) => {
-  // const { name = 'World' } = req.query;
-  const db = mongoose.connection;
-  db.on("error", (err) => {
-    res.status(200).send(JSON.stringify(err));
-  });
-  db.once("open", async () => {
-    const products = await Product.find();
-    res.status(200).send(JSON.stringify(products));
-  });
+    // const { name = 'World' } = req.query;
+    const db = mongoose.connection;
+    db.on('error', (err) => {
+        res.status(200).send(JSON.stringify(err));
+    });
+    db.once('open', async () => {
+        const products = await Product.find();
+        res.status(200).send(JSON.stringify(products));
+    });
 
-  setTimeout(() => {
-    res.status(200).send("Nothing happened");
-  }, 5000);
+    setTimeout(() => {
+        res.status(200).send('Nothing happened');
+    }, 5000);
 };
