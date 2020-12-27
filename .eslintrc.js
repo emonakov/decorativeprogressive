93dea148af4d05fa9aa2e2e17aba47dd1d3d2359
Eugene Monakov <evgenii.monakov@auto1.com>
2020-12-27 23:54:56 +0100

    eslint fix, alias init

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,38 +12,33 @@ module.exports = {
         jasmine: true,
         jest: true,
     },
-    rules: {
-        '@typescript-eslint/explicit-function-return-type': 'off',
-        '@typescript-eslint/no-non-null-assertion': 'off',
-        'import/no-named-as-default': 'off',
-        'import/no-named-as-default-member': 'off',
-        'import/prefer-default-export': 'off',
-        'import/named': 'off',
-        'react/prop-types': 'off',
-        indent: ['error', 4, { SwitchCase: 1 }],
-        'max-len': ['error', { code: 120 }],
-        'newline-before-return': 'error',
-        // Indent JSX with 4 spaces
-        'react/jsx-indent': ['error', 4],
-        // Indent props with 4 spaces
-        'react/jsx-indent-props': ['error', 4],
-        'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
-        'import/extensions': 'off',
-        'react/jsx-props-no-spreading': 'off',
-        '@typescript-eslint/no-var-requires': 'off',
-        '@typescript-eslint/no-explicit-any': 'off',
-        "@typescript-eslint/no-unused-vars": "off",
-        "@typescript-eslint/no-unused-vars-experimental": "error",
-        "no-unused-vars": "off"
-    },
-    settings: {
-        react: {
-            pragma: 'React',
-            version: 'detect',
-        },
-    },
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        project: `./tsconfig.json`
-    },
+    overrides: [
+        {
+            files: ['**/*.ts?(x)'],
+            rules: {
+                '@typescript-eslint/explicit-function-return-type': 'off',
+                '@typescript-eslint/no-non-null-assertion': 'off',
+                'import/no-named-as-default': 'off',
+                'import/no-named-as-default-member': 'off',
+                'import/prefer-default-export': 'off',
+                'import/named': 'off',
+                'react/prop-types': 'off',
+                indent: ['error', 4, { SwitchCase: 1 }],
+                'max-len': ['error', { code: 120 }],
+                'newline-before-return': 'error',
+                // Indent JSX with 4 spaces
+                'react/jsx-indent': ['error', 4],
+                // Indent props with 4 spaces
+                'react/jsx-indent-props': ['error', 4],
+                'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+                'import/extensions': 'off',
+                'react/jsx-props-no-spreading': 'off',
+                '@typescript-eslint/no-var-requires': 'off',
+                '@typescript-eslint/no-explicit-any': 'off',
+                "@typescript-eslint/no-unused-vars": "off",
+                "@typescript-eslint/no-unused-vars-experimental": "error",
+                "no-unused-vars": "off"
+            },
+        }
+    ],
 };
