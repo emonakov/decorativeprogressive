c91f92e02249cbf871a1997b13ff9fb440154cc2
Eugene Monakov <evgenii.monakov@auto1.com>
2020-12-27 18:57:16 +0100

    forgotten files

---
+++ src/components/Pages/Admin/Main.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import { HomePageHero } from '../../Heroes';
+import ContentWrapper from '../../../shared/components/ContentWrapper';
+
+import { useContextState } from '../../../shared/components/StateProvider';
+import { signInWithGoogle, auth } from '../../../firebase/firebase.utils';
+
+const Main: React.FC = () => {
+    const [state] = useContextState();
+
+    return (
+        <>
+            <HomePageHero>
+                <h1>Admin entrance</h1>
+            </HomePageHero>
+            <ContentWrapper>
+                <h1>TEST</h1>
+                {!state.isAuthenticated && <button type="button" onClick={signInWithGoogle}>SIGN IN</button>}
+                {state.isAuthenticated && <button type="button" onClick={() => auth.signOut()}>SIGN OUT</button>}
+            </ContentWrapper>
+        </>
+    );
+};
+
+export default Main;
