8fe51a1c3299b5850df4e5baefcc5cdce73c657e
Eugene Monakov <eugene@gojilabs.com>
2022-03-01 20:35:30 +0300

    Petition

--- src/components/Heroes/HomePageHero.tsx
+++ src/components/Heroes/HomePageHero.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import Frame from '../../shared/components/Frame';
 import Hero from '../../shared/components/Hero';
@@ -7,9 +7,10 @@ import HeroImg from '../../images/homepage-hero.png';
 
 const HomePageHero: React.FC = ({ children, ...props }) => (
     <Hero background={HeroImg} {...props}>
-        <Frame scrollToEl="main">
+        {/* <Frame scrollToEl="main"> */}
+        <Frame>
             {children}
-            <FontAwesomeIcon icon={['fas', 'chevron-down']} size="2x" />
+            {/* <FontAwesomeIcon icon={['fas', 'chevron-down']} size="2x" /> */}
         </Frame>
     </Hero>
 );
--- src/components/Pages/Home.tsx
+++ src/components/Pages/Home.tsx
@@ -6,7 +6,13 @@ import About from '../About';
 const HomePage: React.FC = () => (
     <>
         <HomePageHero>
-            <h1>Decorative Progressive</h1>
+            <h1>
+                We stand for peace and democratic values and condemn any aggression
+                against our fellow Ukrainians. If you share same values and find no
+                justification to the acts of war please sign this
+                {' '}
+                <a href="http://www.change.org/NetVoyne">petition</a>
+            </h1>
         </HomePageHero>
         <About />
     </>
